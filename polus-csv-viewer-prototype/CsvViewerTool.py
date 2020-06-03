f4dc1579cc516954e74636d2a37c059dc8c72604
Melanie Parham <melanie.parham@gmail.com>
2020-06-03 19:03:50 -0400

    docs: Update developer guide to include instructions for deploying on QA #POL-693

--- polus-csv-viewer-prototype/CsvViewerTool.py
+++ polus-csv-viewer-prototype/CsvViewerTool.py
@@ -32,7 +32,7 @@ _create_widgets(): Make widget to select file from collection
 _on_file_clicked(): Display the selected CSV file as a beakerx table
 
 """
-class BeakerXButtons():
+class BeakerXTable():
 
     def __init__(self, file_list: str, file_widget):
         """Set the logger and view filenames in file_list ending in csv
@@ -41,7 +41,7 @@ class BeakerXButtons():
             file_list: Path to shared folder CSV files
             file_widget: Combobox widget that holds CSV files
         """
-
+        
         #: Set up the logger
         logging.basicConfig()
         self.logger = logging.getLogger(__name__)
@@ -75,7 +75,7 @@ class BeakerXButtons():
         self.sel_file.options=filenames_stem
         self.sel_file.placeholder="Choose a CSV file"
         
-        #: Create button to generate a new BeakerX interactive table
+        #: Generate a new BeakerX interactive table
         self.sel_file.observe(self._on_file_clicked, 'value')
 
         
@@ -93,12 +93,13 @@ class BeakerXButtons():
             df = pd.read_csv(csv_full_file_path)
             
             # Check if first pandas row contains only "F" or "C"
-            test = df.iloc[0].isin(["F","C"]).all()
-            
-            # If so, delete first row and convert datatype to float
-            if test == True:
-                df = df.iloc[1:]
-                df = df.astype('float64', errors='raise')
+            if df.size > 0:
+                test = df.iloc[0].isin(["F","C"]).all()
+
+                # If so, delete first row and convert datatype to float
+                if test == True:
+                    df = df.iloc[1:]
+                    df = df.astype('float64', errors='raise')
 
             #: Display dataframe of selected csv file as BeakerX table
             beakerx_table = TableDisplay(df)
@@ -106,7 +107,7 @@ class BeakerXButtons():
             display(HBox([beakerx_table]))
                 
     def display_widgets(self):
-        """Display 'New Table' button & ComboBox for selecting files"""
+        """Display ComboBox for selecting files"""
 
         self._create_widgets()
 
@@ -117,54 +118,64 @@ class BeakerXButtons():
         display(VBox([self.out]))
 
 def get_wipp_csvs():
-        """Access WIPP CI WippPy API to view CSV collection names
+    """Access WIPP CI WippPy API to view CSV collection names
 
-        Returns:
-            Dict: Dict of image collection unique ids and names
-        """
-        #: Set logging level for more details (DEBUG, INFO, WARNING)
-        wippy.logger.setLevel(logging.WARNING)
+    Be sure to change wipp_api_url accordingly, depending on
+    whether you are using the CI or QA deployment of JupyterLab.
 
-        #: URL to WIPP API
-        wipp_api_url = 'http://wipp-ui.ci.aws.labshare.org/api/'
-        wippy.WippData.setWippUrl(wipp_api_url)
+    Returns:
+        Dict: Dict of image collection unique ids and names
+    """
+    #: Set logging level for more details (DEBUG, INFO, WARNING)
+    wippy.logger.setLevel(logging.WARNING)
 
-        #: Return dict of image collection unique ids and names
-        return wippy.WippCsvCollection.all()
+    #: URL to WIPP API
+    wipp_api_url = 'http://wipp-ui.ci.aws.labshare.org/api/' #: CI
+    #wipp_api_url = 'http://wipp-ui.test.aws.labshare.org/api/' #: QA
+
+    wippy.WippData.setWippUrl(wipp_api_url)
+
+    #: Return dict of image collection unique ids and names
+    return wippy.WippCsvCollection.all()
 
 def on_collection_clicked(*args):
-        """When change in collections widget detected, make next widget.
-        """
-        
-        file_widget.index = None
-        
-        #: Clear output
-        output.clear_output()
-        logging.basicConfig()
-        logger = logging.getLogger(__name__)
-        logger.setLevel(logging.DEBUG) 
+    """When change in collections widget detected, make next widget.
+    """
+    file_widget.index = None
 
-        with output:
-            if __name__=="__main__":
-                cid = ""
-                id_csvname_dict = get_wipp_csvs()
-                for each_id, each_csvname in id_csvname_dict.items():
-                    id_csvname_dict[each_id] = str(each_csvname).split()[0]
-                for each_id, each_csvname in id_csvname_dict.items():
-                    if args[0]['new'] == each_csvname:
-                        cid = each_id
-                p = pathlib.Path("../../shared/wipp/csv-collections/")
+    #: Clear output
+    output.clear_output()
+    logging.basicConfig()
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG) 
+
+    with output:
+        if __name__=="__main__":
+            cid = ""
+            id_csvname_dict = get_wipp_csvs()
+            for each_id, each_csvname in id_csvname_dict.items():
+                id_csvname_dict[each_id] = str(each_csvname).split()[0]
+            for each_id, each_csvname in id_csvname_dict.items():
+                if args[0]['new'] == each_csvname:
+                    cid = each_id
+            p = pathlib.Path("../../shared/wipp/csv-collections/")
+            if not p.is_dir():
+                logger.error("The csv-collections directory does not exist in JupyterLab")
+            elif p.is_dir():
                 subdirectories = [x.name for x in p.iterdir() if x.is_dir()]
-                if cid in subdirectories:                    
+
+                if cid in subdirectories: 
+
                     #: Define the path
                     pathlib_directory = pathlib.Path(
                         "../../shared/wipp/csv-collections") / cid
-                    show_files = BeakerXButtons(pathlib_directory, file_widget)
+                    show_files = BeakerXTable(pathlib_directory, file_widget)
                     show_files.display_widgets()
+
                 elif cid not in subdirectories:
-                    logger.error("CSV collections in WIPP not in JupyterLab.")
+                    logger.error("Selected CSV collection not found in JupyterLab.")
                     logger.error(args[0])
-                    
+
                 else:
                     logger.critical("Unknown error.")  
 
--- polus-csv-viewer-prototype/Developer Guide.md
+++ polus-csv-viewer-prototype/Developer Guide.md
@@ -9,21 +9,26 @@ Here are the basic steps you need to set up the code
 Before running the code, run the following commands in the terminal from 
 within JupyterLab:
 
-* jupyter labextension install @jupyter-widgets/jupyterlab-manager
-* jupyter labextension install beakerx-jupyterlab
-* pip install py4j
-* pip install beakerx==1.4.1
-* beakerx install
+* `jupyter labextension install @jupyter-widgets/jupyterlab-manager`
+* `jupyter labextension install beakerx-jupyterlab`
+* `pip install py4j`
+* `pip install beakerx==1.4.1`
+* `beakerx install`
 
 These enable the BeakerX ipywidget to work in JupyterLab.
 
 ## Run the Program
-To run the code directly from JupyterLab CI, install the WippPy API
-from GitHub. Next, save `CsvViewerTool.ipynb` to the `wippy` folder
-from within a local directory. 
+To run the code directly from JupyterLab, clone the Wippy API
+from GitHub. Next, save `CsvViewerTool.ipynb` to the first `wippy` 
+folder from within your workspace directory. 
 
-From there, the GUI can be viewed by selecting 
-`View`>`Render notebook in Voila` from the JupyterLab GUI.
+Locate the function `get_wipp_csvs()`. If you are deploying from 
+JupyterLab CI, no modifications are needed. However, if you are 
+deploying from JupyterLab QA, uncomment the following line:
+`#wipp_api_url = 'http://wipp-ui.test.aws.labshare.org/api/' #: QA`
+
+From there, save the change and view the application by selecting 
+`View`>`Open with Voila in New Browser Tab` from the JupyterLab GUI.
 
 You will be able to select any CSV collection that exists in WIPP CI,
 select a file from within a CSV collection, and visualize the table 
